4a3b513e23554cb987eaada184864e8c07b40297
Javier Urrutia <javier.urrutia.r@ug.uchile.cl>
2020-07-12 14:22:05 -0400

    Created robot_status node

---
+++ robot_status/src/status.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+from kobuki_msgs.msg import SensorState
+from std_msgs.msg import UInt8,Bool
+
+class kobuki_status():
+
+
+	def __init__(self):
+		rospy.init_node('kobuki_status')
+		self.Vmax    = rospy.get_param('~kobuki_base_max_charge', 165)
+		self.Vmin    = rospy.get_param('~kobuki_base_min_charge', 130)   
+		self.conv_factor = 100.0/(self.Vmax - self.Vmin)    
+		rospy.Subscriber('/jaime/mobile_base/sensors/core', SensorState, self.BatteryCallback)
+		self.pub = rospy.Publisher('/jaime/battery_percent', UInt8, queue_size = 10)
+		self.ledon_pub = rospy.Publisher('/jaime/arduino/led/on', Bool, queue_size = 10, latch=True)
+		rospy.sleep(1)
+		self.ledon_pub.publish(True)
+		rospy.spin()
+
+	def BatteryCallback(self, data):
+		battery_percent = round(float(data.battery-self.Vmin) * self.conv_factor )
+		self.pub.publish(battery_percent)
+		return
+
+if __name__ == '__main__':
+	try:
+		kobuki_status()
+	except:
+		rospy.logwarn('Cannot run status')
+
