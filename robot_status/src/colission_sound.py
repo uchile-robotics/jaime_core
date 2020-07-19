e1e0435df8a8ba746b80890278a5a5a92f6fcba5
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2020-07-19 22:38:37 +0000

    add teleop sound

--- robot_status/src/colission_sound.py
+++ robot_status/src/colission_sound.py
@@ -18,6 +18,7 @@ class colission_sound():
     def __init__(self):
         rospy.init_node('collision_sound')
         self.sound = True
+        self.teleop = True
 
         self.distance_min = 4.0
         self.sound_period = 0.5
@@ -28,17 +29,17 @@ class colission_sound():
 
         #Loop
         while not rospy.core.is_shutdown():
-            if self.sound:
+            if self.sound and self.teleop:
                 if self.distance_min<DISTANCE_TH:
                     self.sound_publisher.publish(SOUND_ID)
-                    rospy.sleep(self.sound_period)
+            rospy.sleep(self.sound_period)
                     #print(self.sound_period)
             
 
 
 
     def sound_callback(self,msg):
-        self.sound = msg.data
+        self.teleop = msg.data
         #print(self.sound)
     def laser_callback(self,msg):
         laser = msg.ranges
@@ -48,10 +49,13 @@ class colission_sound():
         if self.distance_min < DISTANCE_TH:
             if self.distance_min > DISTANCE_TH_2:
                 self.sound_period = M * self.distance_min
+                self.sound = True
             else:
                 self.sound_period = MIN_PERIOD
+                self.sound = True
         else:
             self.sound_period = MAX_PERIOD
+            self.sound = False
         return
 if __name__ == '__main__':
     colission_sound()
