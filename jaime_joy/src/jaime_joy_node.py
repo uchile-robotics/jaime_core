419dddecffce2296c4ed9506b7df8ecdbbcf40c2
yosoy2 <edotercero66.el@gmail.com>
2022-06-07 18:13:59 -0400

    factor velocity

--- jaime_joy/src/jaime_joy_node.py
+++ jaime_joy/src/jaime_joy_node.py
@@ -8,7 +8,7 @@ class Jaime_Joy():
     def __init__(self):
         self.subscriber = rospy.Subscriber('/joy', Joy, self._callback)
         self.publisher = rospy.Publisher('/mobile_base/commands/velocity', Twist , queue_size=10)
-        
+
     def _callback(self, msg):
 
         axes = msg.axes
@@ -16,19 +16,19 @@ class Jaime_Joy():
 
         adelante = axes[1]
         girar = axes[3]
-        
+
         button_a = buttons[0]
         button_b = buttons[1]
         button_x = buttons[2]
         button_y = buttons[3]
 
         output = Twist()
-        
-        output.linear.x = adelante 
-        output.angular.z = girar
-        
+
+        output.linear.x = adelante*0.3
+        output.angular.z = girar*1
+
         self.publisher.publish(output)
-            
+
 
 def main():
 
