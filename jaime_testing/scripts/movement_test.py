3f07b13fd88034a340b724c04dc2913c0959a958
Mari <marinaascencio524@gmail.com>
2025-11-06 16:25:48 -0300

    (MA) jaime core basado en bender

--- jaime_testing/scripts/movement_test.py
+++ jaime_testing/scripts/movement_test.py
@@ -9,7 +9,7 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_= self-create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_= self.create_publisher(Twist,'cmd_vel',10)            
         timer_period = 0.01
         self.timer =self.create_timer(timer_period, self.timer_callback)
         self.i=0
