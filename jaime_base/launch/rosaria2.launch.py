97931128a905b6be89f6b2c55f9481c495729e4f
Mari <marinaascencio524@gmail.com>
2025-11-19 15:00:02 -0300

    (MA) modificando docker jaime

--- jaime_base/launch/rosaria2.launch.py
+++ jaime_base/launch/rosaria2.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -10,16 +10,16 @@ def generate_launch_description(*args, **kwargs):
     parameters = [os.path.join(
         get_package_share_directory(pkg),
         'params',
-        'pioneer.yaml'
+        'kobuki.yaml'
     )]
 
     # Create and return the Node
     return LaunchDescription([
         Node(
             namespace='',
-            package='rosaria2',
-            executable='rosaria2_node',
-            name='rosaria2',
+            package='kobuki',
+            executable='kobuki_node',
+            name='kobuki',
             parameters=[parameters],
         )
     ])
