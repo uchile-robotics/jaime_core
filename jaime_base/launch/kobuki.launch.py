b3c405990bedd8e56ce44217b82bd0580076f61f
Mari <marinaascencio524@gmail.com>
2025-11-28 13:10:24 -0300

    (MA) editando kobuki launch

--- jaime_base/launch/kobuki.launch.py
+++ jaime_base/launch/kobuki.launch.py
@@ -13,13 +13,35 @@ def generate_launch_description(*args, **kwargs):
         'kobuki.yaml'
     )]
 
-    # Create and return the Node
-    return LaunchDescription([
-        Node(
-            namespace='',
-            package='kobuki',
-            executable='kobuki_node',
-            name='kobuki',
-            parameters=[parameters],
-        )
-    ])
+    ld = LaunchDescription()
+
+    kobuki_cmd = Node(
+        package='kobuki_node',
+        executable='kobuki_ros_node',
+        namespace=LaunchConfiguration('namespace'),
+        output='screen',
+        parameters=[kobuki_params],
+        remappings=[
+            ('/commands/velocity', '/cmd_vel')
+        ]
+    )
+
+    tf_footprint2base_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        namespace='',
+        output='screen',
+        remappings=[
+            ('/tf', 'tf'), ('/tf_static', 'tf_static')
+        ],
+        arguments=[
+            '0.0', '0.0', '0.001',
+            '0.0', '0.0', '0.0',
+            '1.0', 'base_link', 'base_footprint'
+        ],
+    )
+
+    ld.add_action(kobuki_cmd)
+    ld.add_action(tf_footprint2base_cmd)
+
+    return ld
